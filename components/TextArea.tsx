c9075ef092f4f52bd3e282735d8be0c716695bec
Ido Evergreen <dicksonevergreenido@gmail.com>
2022-03-05 23:46:39 +0100

    added paste from clipboard

--- components/TextArea.tsx
+++ components/TextArea.tsx
@@ -1,14 +1,41 @@
 import React from "react";
-
+import { FiMic } from "react-icons/fi";
+import { BsKeyboard } from "react-icons/bs";
 const TextArea = () => {
+  const [value, setValue] = React.useState("hello");
+
+  const pasteFromClipboard = () => {
+    navigator.clipboard.readText().then((text) => {
+      setValue(text);
+    });
+  };
+
   return (
-    <div className="textarea__container shadow-2xl rounded-xl p-5">
-      <textarea
-        className="bg-white border-0  text-2xl  appearance-none focus:outline-none   px-4 py-2 pr-8  shadow-md"
-        rows="5"
-        cols="50"
-        placeholder="Enter your text here"
-      ></textarea>
+    <div className="textarea__container flex justify-center items-center shadow-2xl rounded-3xl p-5 bg-white">
+      <div className="">
+        <textarea
+          className="bg-white resize-none border-0 placeholder-[#BBBBBB]  text-2xl  appearance-none focus:outline-none   px-4 py-2 pr-8  "
+          rows={5}
+          value={value}
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+          cols={50}
+          placeholder="Enter text here"
+        ></textarea>
+
+        <div
+          onClick={pasteFromClipboard}
+          className="p-2 items-center text-sm flex w-44 cursor-pointer space-x-10 justify-between rounded-full  text-[#9C9C9C] bg-[#F2F2F2]"
+        >
+          <BsKeyboard />
+          Paste from clipboard
+        </div>
+      </div>
+
+      <div className=" self-start pt-3">
+        <FiMic className="text-xl text-[#BBBBBB] cursor-pointer" />
+      </div>
     </div>
   );
 };
